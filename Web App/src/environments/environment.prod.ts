c250c740cf32cae1f8a2d0151f8d36705476770b
AzureAD\KeithGandari <keith@istratgo.com>
2026-03-17 17:59:39 +0200

    fix: update contract address in environment configuration

--- Web App/src/environments/environment.prod.ts
+++ Web App/src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   blockchain: {
     network: 'Sepolia',
     rpcUrl: 'https://sepolia.infura.io/v3/4e2f8725145b45ce8c81b664d4e9d20e',
-    contractAddress: '0x29baF19FF34fcf90a8980AD34C3389E8BE3A501E',
+    contractAddress: '0x80f092C834cbc6B583DFBb93bf8FAA92E83DB831',
     chainId: 11155111,
     explorerUrl: 'https://sepolia.etherscan.io'
   }
